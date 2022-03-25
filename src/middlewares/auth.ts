0d67bba0994fdad3a1dcf4a56cd0378027703626
Mohamed Walid <mohamed.walid1999@gmail.com>
2022-03-25 17:14:02 +0200

    fix:bearer token auth

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,7 +1,7 @@
 import jwtService from '@services/jwtService';
 import Exception from '@exceptions/Exception';
 export default async function auth(req, res, next) {
-  const token = req.headers['authorization'];
+  const token = req.headers['authorization']?.split(' ')[1];
   if (!token) next(new Exception('please login', 401));
   else {
     try {
