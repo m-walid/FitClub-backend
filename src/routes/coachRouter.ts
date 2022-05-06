b3d0bc8c1d1d0566bde1844d7552b9b7946d6eb2
Mohamed Walid <mohamed.walid1999@gmail.com>
2022-05-06 20:48:09 +0200

    FIx allow trainee to get coach programs

--- src/routes/coachRouter.ts
+++ src/routes/coachRouter.ts
@@ -10,7 +10,7 @@ coachRouter.post('/', [auth, roleAuth(Role.COACH), CoachController.addCoach]);
 coachRouter.get('/:id', [auth, CoachController.getCoach]);
 coachRouter.patch('/:id', [auth, CoachController.updateCoach]);
 coachRouter.get('/:id/exercises', [auth, roleAuth(Role.COACH), CoachController.getExercises]);
-coachRouter.get('/:id/programs', [auth, roleAuth(Role.COACH), CoachController.getPrograms]);
+coachRouter.get('/:id/programs', [auth, CoachController.getPrograms]);
 coachRouter.post('/:id/reviews', [auth, roleAuth(Role.TRAINEE), CoachController.postReview]);
 coachRouter.get('/:id/reviews', [auth, CoachController.getReviews]);
 coachRouter.patch('/reviews/:id', [auth, CoachController.updateReview]);
